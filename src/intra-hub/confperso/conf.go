c864312fe86fafad4d73f209f08138fd166782bd
solign_t <solignac.t@gmail.com>
2015-05-27 13:12:59 +0000

    Move personnal configuration

---
+++ src/intra-hub/confperso/conf.go
@@ -0,0 +1,10 @@
+package confperso
+
+// To update for yourself. Don't commit it
+
+const (
+    AliasDbName                = "default"
+	DatabaseName               = "intra_hub"
+	Username                   = "root"
+	Password                   = ""
+)
