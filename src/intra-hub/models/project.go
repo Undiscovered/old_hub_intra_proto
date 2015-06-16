ee38923ffd663a0f6cda8025fdeb82d9f42caeab
Vincent <20cent.neel@gmail.com>
2015-06-16 16:08:47 +0200

    switched images

--- src/intra-hub/models/project.go
+++ src/intra-hub/models/project.go
@@ -8,6 +8,7 @@ import (
 	"github.com/astaxie/beego/orm"
 	"github.com/astaxie/beego/validation"
 	"github.com/docker/docker/pkg/stringutils"
+	"github.com/saschpe/tribool"
 )
 
 func init() {
@@ -16,16 +17,16 @@ func init() {
 
 type Project struct {
 	Id                     int
-	Name                   string         `json:"name" orm:"unique;size(128)" form:"name"`
-	ShortDescription       string         `json:"shortDescription" orm:"size(128)" form:"shortDescription"`
-	Status                 *ProjectStatus `json:"status" orm:"rel(fk)"`
-	Manager                *User          `json:"manager" orm:"null;rel(fk)"`
-	History                []*HistoryItem `json:"history" orm:"null;rel(m2m)"`
-	Members                []*User        `json:"members" orm:"null;reverse(many)"`
-	Themes                 []*Theme       `json:"themes" orm:"null;rel(m2m)"`
-	Created                time.Time      `json:"created" orm:"auto_now_add;type(datetime)"`
-	Updated                time.Time      `json:"updated" orm:"auto_now;type(datetime)"`
-	PedagogicallyValidated bool           `json:"pedagogicallyValidated" form:pedagogicallyValidated"`
+	Name                   string          `json:"name" orm:"unique;size(128)" form:"name"`
+	ShortDescription       string          `json:"shortDescription" orm:"size(128)" form:"shortDescription"`
+	Status                 *ProjectStatus  `json:"status" orm:"rel(fk)"`
+	Manager                *User           `json:"manager" orm:"null;rel(fk)"`
+	History                []*HistoryItem  `json:"history" orm:"null;rel(m2m)"`
+	Members                []*User         `json:"members" orm:"null;reverse(many)"`
+	Themes                 []*Theme        `json:"themes" orm:"null;rel(m2m)"`
+	Created                time.Time       `json:"created" orm:"auto_now_add;type(datetime)"`
+	Updated                time.Time       `json:"updated" orm:"auto_now;type(datetime)"`
+	PedagogicallyValidated tribool.Tribool `json:"pedagogicallyValidated" form:pedagogicallyValidated"`
 
 	// Non Persistent fields
 
--- src/intra-hub/models/projectStatus.go
+++ src/intra-hub/models/projectStatus.go
@@ -3,22 +3,20 @@ package models
 import "github.com/astaxie/beego/orm"
 
 const (
-	ProjectStatusWaitingManager              = "WAITING_MANAGER"
-	ProjectStatusToFormalize                 = "TO_FORMALIZE"
-	ProjectStatusPedagogicalValidationNeeded = "PEDAGOGICAL_VALIDATION_NEEDED"
-	ProjectStatusPitchRequired               = "PITCH_REQUIRED"
-	ProjectStatusInProgress                  = "IN_PROGRESS"
-	ProjectStatusWaitingForOralDefense       = "WAITING_FOR_DEFENSE"
-	ProjectStatusCreditsGiven                = "CREDITS_GIVEN"
-	ProjectStatusBlockedByPedagogy           = "BLOCKED_BY_PEDAGOGY"
-	ProjectStatusAbandoned                   = "ABANDONED"
+	ProjectStatusWaitingManager        = "WAITING_MANAGER"
+	ProjectStatusToFormalize           = "TO_FORMALIZE"
+	ProjectStatusPitchRequired         = "PITCH_REQUIRED"
+	ProjectStatusInProgress            = "IN_PROGRESS"
+	ProjectStatusWaitingForOralDefense = "WAITING_FOR_DEFENSE"
+	ProjectStatusCreditsGiven          = "CREDITS_GIVEN"
+	ProjectStatusAbandoned             = "ABANDONED"
 )
 
 var (
 	EveryProjectStatus = []string{ProjectStatusWaitingManager, ProjectStatusToFormalize,
-		ProjectStatusPedagogicalValidationNeeded, ProjectStatusPitchRequired,
-		ProjectStatusInProgress, ProjectStatusWaitingForOralDefense, ProjectStatusCreditsGiven,
-		ProjectStatusBlockedByPedagogy, ProjectStatusAbandoned}
+		ProjectStatusPitchRequired, ProjectStatusInProgress,
+        ProjectStatusWaitingForOralDefense, ProjectStatusCreditsGiven,
+		ProjectStatusAbandoned}
 )
 
 func init() {
