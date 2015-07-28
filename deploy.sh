56b56dd17b2560e45a41dd56130ee7b0689e95cd
Vincent Neel <20cent.neel@gmail.com>
2015-07-28 14:44:15 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 git pull
-cd /home/neel_v/hub_intra_proto/src/intra-hub
+cd $HOME/hub_intra_proto/src/intra-hub
 bee pack
+sudo mkdir -p /var/www/beego
 sudo rm -rfv /var/www/beego/*
 sudo mv -v intra-hub.tar.gz /var/www/beego
 cd /var/www/beego
 sudo tar -xvf intra-hub.tar.gz
 sudo rm -v intra-hub.tar.gz
 sudo pkill intra-hub
-echo '' > /home/neel_v/nohup.out
+echo '' > $HOME/nohup.out
 sudo nohup ./intra-hub&
