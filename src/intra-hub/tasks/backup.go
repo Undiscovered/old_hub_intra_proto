a52c66902156b75963553b2318ef014faf1b89bc
Vincent <20cent.neel@gmail.com>
2015-07-29 14:41:57 +0200

    backup done

--- src/intra-hub/tasks/backup.go
+++ src/intra-hub/tasks/backup.go
@@ -1,12 +1,30 @@
 package tasks
 
-import "github.com/astaxie/beego/toolbox"
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/toolbox"
+	"intra-hub/confperso"
+	"os"
+	"os/exec"
+	"time"
+)
 
 const (
 	backupTaskName = "Backup"
 )
 
 func backup() error {
+	fileName := time.Now().Format(time.RFC3339) + ".sql"
+	outputDirectoryName := os.Getenv("HOME") + "/backup"
+	os.Mkdir(outputDirectoryName, 0700)
+	outputFileName := outputDirectoryName + "/" + fileName
+	// generates something like mysqldump -u root --p="password" intra_hub > /Users/Vincent/backup/2015-07-29T13:58:36+02:00.sql
+	cmd := "mysqldump -u " + confperso.Username + " --password=\"" + confperso.Password + "\" " + confperso.DatabaseName + " > " + outputFileName
+	out, err := exec.Command("bash", "-c", cmd).CombinedOutput()
+	if err != nil {
+		beego.Warn(string(out))
+		return err
+	}
 	return nil
 }
 
